a59234c6c20c87d3528fe305ca7e0eac02d7e2c8
Pierre Majerus <pierre.majerus@outlook.be>
2024-02-14 21:49:05 +0100

    To check JS LINK accueil/habitat

--- js/habitats.js
+++ js/habitats.js
@@ -49,14 +49,55 @@ btns.forEach((btn, index) => {
 
 
 // Get destination animals 
-let recup = document.getElementById('bison')
-let test = window.location.hash;
-let rico = document.getElementById('vache');
+const fetchTortule = document.getElementById('tortue');
+const fetchBird = document.getElementById('oiseau');
+const fetchCow = document.getElementById('vache');
+const fetchKango = document.getElementById('kango');
+const fetchBear = document.getElementById('ours');
+const fetchCameleon = document.getElementById('cameleon');
+const fetchPanda = document.getElementById('panda');
+const fetchMonkey = document.getElementById('gorille');
 
-let sas = window.location.hash.substring(1);
+const getFarm = document.getElementById('getClickFarm');
+const getOcean = document.getElementById('getClickOcean');
+const getPond = document.getElementById('getClickPond');
+const getTaniere = document.getElementById('getClickTaniere');
+const getVivarium = document.getElementById('getClickVivarium');
+const getBird = document.getElementById('getClickBird');
 
-if (sas == 'vache') {
-rico.innerText = 'Cliquez ici';
-  recup.style.display = 'block';
+let url = window.location.hash.substring(1);
+
+switch(url) {
+  case 'vache' :
+  getFarm.style.display = 'block';
+  fetchCow.style.display = 'block';
+  break
+  case 'ours' :
+    getTaniere.style.display = 'block';
+    fetchBear.style.display = 'block';
+  break
+  case 'tortue' :
+    getOcean.style.display = 'block';
+    fetchTortule.style.display = 'block';
+  break
+  case 'panda' :
+    getTaniere.style.display = 'block';
+    fetchPanda.style.display = 'block';
+  break
+  case 'oiseau' :
+    getBird.style.display = 'block';
+    fetchBird.style.display = 'block';
+  break
+  case 'gorille' :
+    getTaniere.style.display = 'block';
+    fetchMonkey.style.display = 'block';
+  break
+  case 'kango' :
+    getTaniere.style.display = 'block';
+    fetchKango.style.display = 'block';
+  break
+  case 'cameleon' :
+    getVivarium.style.display = 'block';
+    fetchCameleon.style.display = 'block';
+  break
 }
-

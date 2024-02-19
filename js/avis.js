72a5a3d33bffa59f6caa37d5e4de2236169e5654
Pierre Majerus <pierre.majerus@outlook.be>
2024-02-19 11:52:54 +0100

    finished pages

--- js/avis.js
+++ js/avis.js
@@ -13,31 +13,28 @@ links.forEach((link) => {
 
 // Récupération avis form
 
-let form = document.getElementById('form');
+// let form = document.getElementById('form');
 
-form.addEventListener('submit', function(e) {
-  let name = document.getElementById('name').value;
-  let email = document.getElementById('email').value;
-  let avisTrain = document.getElementById('avis_train').value;
-  let avisRestaurant = document.getElementById('avis_restaurant').value;
-  let avisVisite = document.getElementById('avis_visite').value;
-  let avisDivers = document.getElementById('avis_divers').value;
+// form.addEventListener('submit', function(e) {
+//   let name = document.getElementById('name').value;
+//   let email = document.getElementById('email').value;
+//   let avisTrain = document.getElementById('avis_train').value;
+//   let avisRestaurant = document.getElementById('avis_restaurant').value;
+//   let avisVisite = document.getElementById('avis_visite').value;
+//   let avisDivers = document.getElementById('avis_divers').value;
 
 
-  let regex = /^[a-zA-Z-\s]+$/; //allow 1 space, Up and Lowercase
+//   let regex = /^[a-zA-Z-\s]+$/;
   
-  if (name.value.trim() == '') {
-    let error = document.getElementById('error');
-    error.innerHTML = "Le champs prénom est requis.";
-    error.style.color = 'red';
-    e.preventDefault();
-  } else if (regex.test(name.value) == false) {
-    let error = document.getElementById('error');
-    error.innerHTML = "Le prénom de peut comporter que des lettres et un '-'.";
-    error.style.color = 'red';
-    e.preventDefault();
-  }
-  
-  
-
-})
+//   if (name.value.trim() == '') {
+//     let error = document.getElementById('error');
+//     error.innerHTML = "Le champs prénom est requis.";
+//     error.style.color = 'red';
+//     e.preventDefault();
+//   } else if (regex.test(name.value) == false) {
+//     let error = document.getElementById('error');
+//     error.innerHTML = "Le prénom de peut comporter que des lettres et un '-'.";
+//     error.style.color = 'red';
+//     e.preventDefault();
+//   }
+// })

86e621f4302964a30745f97f168c800020d5b874
Pierre Majerus <pierre.majerus@outlook.be>
2024-02-26 16:04:04 +0100

    JS contact ok

--- js/avis.js
+++ js/avis.js
@@ -21,44 +21,36 @@ form.addEventListener('submit', (e) => {
   let usernameValue = username.value.trim();
   let regexName = /^[a-zA-Z-\s]+$/;
   let errorName = document.getElementById('errorName');
-  errorName.style.color = 'red';
+  let descriptionValue = description.value.trim();
+  let regexDescription = /^[a-zA-Z0-9\s.,;:'"!?-]*$/;
+  let maxwords = descriptionValue.split(/\s+/).length;
+  let errorDesc = document.getElementById('errorDesc');
+
 
   // Validation name
+  
   if(usernameValue === '') {
     errorName.innerHTML = 'Vous devez avoir un prénom.';
     e.preventDefault(); // Empeche soumission du formulaire
   } else if (usernameValue.length > 30) {
-    errorName.innerHTML = 'Votre prénom est trop long.'; 
+    errorName.innerHTML = 'Votre prénom est trop long.';
+    username.value = '';
     e.preventDefault(); 
   } else if (usernameValue.length < 2) {
     errorName.innerHTML = 'Votre prénom doit contenir au moins 2 lettres.'; 
     e.preventDefault(); 
   } else if (!regexName.test(usernameValue)) {
-    errorName.innerHTML = 'Votre prénom ne peut comporter que des lettres ou des tirets.'; 
+    errorName.innerHTML = 'Votre prénom ne peut comporter que des lettres ou des tirets.';
     e.preventDefault(); 
   }
 
-
-
-/* 
-ainsi que des petits bug (lorsque c 'est bon toujours le message d'erreur) 
-
-
-*/
-
-  let descriptionValue = description.value.trim();
-  let regexDescription = /^[a-zA-Z0-9\s.,;:'"!?-]*$/;
-  let maxwords = descriptionValue.split(/\s+/).length;
-  let errorDesc = document.getElementById('errorDesc');
-  errorDesc.style.color = 'red';
-
-
   // Validation description
+
   if (descriptionValue === ''){
     errorDesc.innerHTML = 'Vous devez écrire un avis pour envoyer le formulaire.'; 
     e.preventDefault(); // Empeche soumission du formulaire
   } else if (!regexDescription.test(descriptionValue)) {
-    errorDesc.innerHTML = 'Vous ne pouvez utiliser que des chiffres, des lettres ou \' , . : ; ! ?'; 
+    errorDesc.innerHTML = 'Vous ne pouvez utiliser que des chiffres, des lettres ou des points de ponctuactions'; 
     e.preventDefault(); 
 } else if (maxwords > 1000) {
   errorDesc.innerHTML = 'Vous devez écrire - de 1000 mots.'; 
--- js/contact.js
+++ js/contact.js
@@ -1,3 +1,5 @@
+// Bar navigation responsive "icon".
+
 const links = document.querySelectorAll('nav li');
 
 icon.addEventListener("click", () => {
@@ -10,3 +12,69 @@ links.forEach((link) => {
   });
 });
 
+
+// Condition form
+
+let form = document.getElementById('form');
+let title = document.getElementById('title')
+let email = document.getElementById('email');
+let description = document.getElementById('contact-description');
+
+form.addEventListener('submit', (e) => {
+  let titleValue = title.value.trim();
+  let emailValue = email.value.trim();
+  let descValue = description.value.trim();
+  let regexTitle = /^[a-zA-Z0-9\s.,;:'"!?-]*$/;
+  let regexEmail = /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/;
+  let maxwords = descValue.split(/\s+/).length;
+  let errorTitle = document.getElementById('errorTitle');
+  let errorEmail = document.getElementById('errorEmail');
+  let errorDesc = document.getElementById('errorDescr');
+
+  // Validation title
+
+  if(titleValue === '') {
+    errorTitle.innerHTML = 'Vous devez avoir un titre.';
+    e.preventDefault(); // Empeche soumission du formulaire
+  } else if (titleValue.length > 30) {
+    errorTitle.innerHTML = 'Votre titre est trop long.';
+    title.value = '';
+    e.preventDefault(); 
+  } else if (titleValue.length < 2) {
+    errorTitle.innerHTML = 'Votre titre doit contenir au moins 2 lettres.'; 
+    e.preventDefault(); 
+  } else if (!regexTitle.test(titleValue)) {
+    errorTitle.innerHTML = 'Votre titre ne peut comporter que des lettres, des chiffres ou des points de ponctuations.';
+    e.preventDefault(); 
+  }
+
+  // Validation email 
+
+  if(emailValue === '') {
+    errorEmail.innerHTML = 'Vous devez avoir une adresse mail.';
+    e.preventDefault(); 
+  } else if (emailValue.length > 30) {
+    errorEmail.innerHTML = 'Votre adresse mail est trop longue.';
+    email.value = '';
+    e.preventDefault(); 
+  } else if (!regexEmail.test(emailValue)) {
+    errorEmail.innerHTML = 'Adresse mail invalide.';
+    e.preventDefault(); 
+  }
+
+  // Validation description
+
+  if (descValue === ''){
+    errorDesc.innerHTML = 'Vous devez écrire un avis pour envoyer le formulaire.'; 
+    e.preventDefault();
+  } else if (!regexTitle.test(descValue)) {
+    errorDesc.innerHTML = 'Vous ne pouvez utiliser que des chiffres, des lettres ou des points de ponctuactions'; 
+    e.preventDefault(); 
+} else if (maxwords > 1000) {
+  errorDesc.innerHTML = 'Vous devez écrire - de 1000 mots.'; 
+  e.preventDefault(); 
+} else if (descValue.length > 6000) {
+  errorDesc.innerHTML = 'Il y a trop de caractères.'; 
+  e.preventDefault(); 
+}
+});

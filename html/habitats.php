573fe301f8edb802f95bc8475af67434ee623c2b
Pierre Majerus <pierre.majerus@outlook.be>
2024-03-27 21:19:26 +0100

    services almost ok

--- html/habitats.php
+++ html/habitats.php
@@ -67,13 +67,37 @@ $sethoraires = $horaires->fetchAll(PDO::FETCH_ASSOC);
     // Fetch data animals
     function getAnimalsData($pdo)
     {
-      $itemsAnimals = 'SELECT animals.name AS prenom, animals.type, animals.race, locations.NAME AS pays, states.state, states.detail,
-       foods.food, foods.grams, foods.date_pass AS passage, animals.root, animals.commonName AS categorie, animals.id_home
-      FROM animals
-      INNER JOIN homes ON homes.id_home = animals.id_home
-      INNER JOIN locations ON locations.id_location = animals.id_location
-      INNER JOIN foods ON foods.id_animal = animals.id_animal
-      INNER JOIN states ON states.id_animal = animals.id_animal';
+      // INNER JOIN SEULEMENT si je veux les animaux dans les 4 tableaux  
+      //   SELECT animals.name AS prenom, animals.type, animals.race, locations.NAME AS pays, states.state, states.detail,
+      //   foods.food, foods.grams, foods.date_pass AS passage, animals.root, animals.commonName AS categorie, animals.id_home
+      //  FROM animals
+      //  INNER JOIN homes ON homes.id_home = animals.id_home
+      //  INNER JOIN locations ON locations.id_location = animals.id_location
+      //  INNER JOIN foods ON foods.id_animal = animals.id_animal
+      //  INNER JOIN states ON states.id_animal = animals.id_animal
+      $itemsAnimals = 'SELECT 
+      animals.name AS prenom, 
+      animals.type, 
+      animals.race, 
+      locations.NAME AS pays, 
+      states.state, 
+      states.detail,
+      foods.food, 
+      foods.grams, 
+      foods.date_pass AS passage, 
+      animals.root, 
+      animals.commonName AS categorie, 
+      animals.id_home
+  FROM 
+      animals
+  LEFT JOIN 
+      homes ON homes.id_home = animals.id_home
+  LEFT JOIN 
+      locations ON locations.id_location = animals.id_location
+  LEFT JOIN 
+      foods ON foods.id_animal = animals.id_animal
+  LEFT JOIN 
+      states ON states.id_animal = animals.id_animal;';
       $stmt = $pdo->prepare($itemsAnimals);
       $stmt->execute();
       $animalsData = $stmt->fetchAll(PDO::FETCH_ASSOC);
--- html/services.php
+++ html/services.php
@@ -51,7 +51,35 @@ $sethoraires = $horaires->fetchAll(PDO::FETCH_ASSOC);
 
   function getServices($pdo)
   {
-    $itemsServices = 'SELECT * FROM services';
+    // Seulement si je veux recuperer ceux de 3 tableaux (Inner join)
+    // SELECT services.main_title, services.second_title, services.img_root AS main_img, services.content, 
+    // services.third_title, services.second_content, services.NAME AS name, services.link_services AS id_link, services.btn_services AS id_btn,
+    // btn_services.classes AS btn_classes, btn_services.link_url AS btn_link_url, btn_services.title_btn, link_services.classes AS link_classes, 
+    // link_services.link_url, link_services.img_root AS link_img_root FROM services 
+    // INNER JOIN link_services ON link_services.id_link = services.link_services,
+    // INNER JOIN btn_services ON btn_services.id_btn = services.btn_services
+    $itemsServices = 'SELECT 
+    services.main_title, 
+    services.second_title, 
+    services.img_root AS main_img, 
+    services.content, 
+    services.third_title, 
+    services.second_content, 
+    services.NAME AS name, 
+    services.link_services AS id_link, 
+    services.btn_services AS id_btn,
+    btn_services.classes AS btn_classes, 
+    btn_services.link_url AS btn_link_url, 
+    btn_services.title_btn, 
+    link_services.classes AS link_classes, 
+    link_services.link_url, 
+    link_services.img_root AS link_img_root 
+FROM 
+    services 
+LEFT JOIN 
+    link_services ON link_services.id_link = services.link_services
+LEFT JOIN 
+    btn_services ON btn_services.id_btn = services.btn_services;';
     $stmt = $pdo->prepare($itemsServices);
     $stmt->execute();
     $serviceData = $stmt->fetchAll(PDO::FETCH_ASSOC);
@@ -76,87 +104,102 @@ $sethoraires = $horaires->fetchAll(PDO::FETCH_ASSOC);
       </div>
     </div>
     <div class="main_div row">
+      <?php foreach ($services as $service) : ?>
+        <section class="main_section col-12 col-sm-6 col-md-4" id="<?php echo $service['name']; ?>">
+          <h3 class="main_h3"><?php echo $service['main_title']; ?></h3>
+          <img src="<?php echo $service['main_img']; ?>" alt="<?php echo $service['name']; ?>" width="100%">
+          <h5 class="main_h5"><?php echo $service['second_title']; ?></h5>
+          <p class="pt-3"><?php echo $service['content']; ?></p>
+          <h5 class="main_h5"><?php echo $service['third_title']; ?></h5>
+          <p class="pt-3"><?php echo $service['second_content']; ?></p>
+          <div class="row">
+            <?php if ($service['link_url'] !== null) : ?>
+              <div class="<?php echo $service['link_classes']; ?>">
+                <a class="main_a" href="<?php echo $service['link_url']; ?>">
+                  <img src="<?php echo $service['link_img_root']; ?>" width="200px" style="border-radius: 20px;">
+                </a>
+              </div>
+            <?php endif; ?>
+            <?php if ($service['btn_link_url'] !== null) : ?>
+              <div class="<?php echo $service['btn_classes']; ?>">
+                <a class="main_a " href="<?php echo $service['btn_link_url']; ?>" target="_blank">
+                  <button class="btn btn-success"><?php echo $service['title_btn']; ?></button>
+                </a>
+              </div>
+            <?php endif; ?>
+          </div>
+        </section>
+      <?php endforeach; ?>
 
 
-
-      <section class="main_section col-12 col-sm-6 col-md-4" id="train">
-        <h3 class="main_h3">Petit tour en train</h3>
-        <div class="img_train row">
-          <img src="../img/services/train.jpg" title="train" width="100%">
-          <h5 class="main_h5">Tarifs pour 45min</h5>
-          <p>
-            -> Adulte: 8€
-            <br />
-            -> Enfant de 4 à 12ans: 5€
-            <br />
-            -> Enfant de - 4ans: Gratuit
-            <br />
-            -> Place réservé pour personne à mobilité reduite
-          </p>
-          <h5 class="main_h5">Durée</h5>
-          <p>
-            Le train roulera de 11h à 17h
-            La promenade dure +/- 45min.
-            Le départ se fera à coté de l'accueil principal.
-            Elle démarrera a toutes les heures pleines de 11h à 18h du mercredi au dimanche.
-            En fonction des conditions climatique, il est possible que le petit train reste au chaud. L'accueil vous
-            préviendra lors de votre arrivé.
-          </p>
-        </div>
+      <!-- <section class="main_section col-12 col-sm-6 col-md-4" id="train">
+        <img src="../img/services/train.jpg" alt="train" width="100%">
+        <h5 class="main_h5">Tarifs pour 45min</h5>
+        <p>
+          -> Adulte: 8€
+          <br />
+          -> Enfant de 4 à 12ans: 5€
+          <br />
+          -> Enfant de - 4ans: Gratuit
+          <br />
+          -> Place réservé pour personne à mobilité reduite
+        </p>
+        <h5 class="main_h5">Durée</h5>
+        <p>
+          Le train roulera de 11h à 17h
+          La promenade dure +/- 45min.
+          Le départ se fera à coté de l'accueil principal.
+          Elle démarrera a toutes les heures pleines de 11h à 18h du mercredi au dimanche.
+          En fonction des conditions climatique, il est possible que le petit train reste au chaud. L'accueil vous
+          préviendra lors de votre arrivé.
+        </p>
       </section>
       <section class="main_section col-12 col-sm-6 col-md-4" id="habitat">
-        <h3 class="main_h3">Visite des habitats (gratuit)</h3>
-        <div>
-          <img src="../img/services/guide.jpg" alt="guide" width="100%">
-          <p class="pt-3">Rendez-vous avec notre guide Arcadia tous les jours de la semaine de 10h30 à 12h et de 14h à
-            16h.</p>
-          <p>Vous aurez la chance d'accéder à notre volière où vivent des centaines de différentes sortent
-            d'oiseaux.</p>
-          <p>Vous découvrirez sous un autre angle les animaux australiens comme le kangourous, ainsi que
-            quelques animaux africains comme le samïri, la
-            girafe ou les gorilles.
-          </p>
-          <p>
-            <svg xmlns="http://www.w3.org/2000/svg" width="20px" viewBox="0 0 512 512"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.-->
-              <path d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zm0-384c13.3 0 24 10.7 24 24V264c0 13.3-10.7 24-24 24s-24-10.7-24-24V152c0-13.3 10.7-24 24-24zM224 352a32 32 0 1 1 64 0 32 32 0 1 1 -64 0z" />
-            </svg>
-            Il est <b>strictement interdit </b> de donner de la nourriture aux animaux pendant la visite. La nourriture
-            est soigneusement préparée et pesée par nos soigneurs. Une quantité trop élévé de nourriture non autorisé
-            peut blesser l'animal, même pire dans certain cas. Merci d'écouter attentivement les consignes de sécurité
-            du guide durant la visite.
-          </p>
+        <h3 class="main_h3">Visite des habitats</h3>
+        <img src="../img/services/guide.jpg" alt="habitat" width="100%">
+        <p class="pt-3">Rendez-vous avec notre guide Arcadia tous les jours de la semaine de 10h30 à 12h et de 14h à
+          16h.
+          Vous aurez la chance d'accéder à notre volière où vivent des centaines de différentes sortent
+          d'oiseaux.
+          Vous découvrirez sous un autre angle les animaux australiens comme le kangourous, ainsi que
+          quelques animaux africains comme les samïris, la
+          girafe ou les gorilles. <b>Activité gratuite !</b>
+        </p>
+        <p>
+          Il est <b>strictement interdit </b> de donner de la nourriture aux animaux pendant la visite. La nourriture
+          est soigneusement préparée et pesée par nos soigneurs. Une quantité trop élévé de nourriture non autorisé
+          peut blesser l'animal, même pire dans certain cas. Merci d'écouter attentivement les consignes de sécurité
+          du guide durant la visite.
+        </p>
 
-        </div>
       </section>
       <section class="main_section col-12 col-md-4" id="resto">
         <h3 class="main_h3">Restaurant</h3>
-        <div>
-          <img src="../img/services/restaurant.jpg" alt="restaurant" width="100%">
-          <p class="pt-3">
-            Notre magnifique restaurant avec 200 places intérieur et 50 places extérieur avec vue sur notre lac, vous
-            accompagne de 11h à 15h tous les jours de la semaine.
-          </p>
-          <p>
-            Nous vous proposons une cuisine 0 déchet et où nous favorisons les produits locaux. Des assiettes saine,
-            riche et savoureuse préparé par nos chefs
-            Le restaurant ressemble à notre physionomie: Mettre tout en notre pouvoir pour contribuer à un planète plus
-            verte pour le biens de nos animaux.
-          </p>
-        </div>
+        <img src="../img/services/restaurant.jpg" alt="resto" width="100%">
+        <p class="pt-3">
+          Notre magnifique restaurant avec 200 places intérieur et 50 places extérieur avec vue sur notre lac, vous
+          accompagne de 11h à 15h tous les jours de la semaine.
+        </p>
+        <p>
+          Nous vous proposons une cuisine 0 déchet et où nous favorisons les produits locaux. Des assiettes saine,
+          riche et savoureuse préparé par nos chefs
+          Le restaurant ressemble à notre physionomie: Mettre tout en notre pouvoir pour contribuer à un planète plus
+          verte pour le biens de nos animaux.
+        </p>
         <div class="row">
           <div class="col-6 col-md-12 col-xl-6 d-flex align-items-center justify-content-center p-4">
             <a class="main_a" href="./terre.php">
-              <img id="terre_verte" src="../img/accueil/ecologie.jpg" alt="eco" width="200px">
+              <img src="../img/accueil/ecologie.jpg" width="200px" style="border-radius: 20px;">
             </a>
           </div>
-          <div class="col-6 col-md-12 col-xl-6 d-flex align-items-center justify-content-center">
+          <div class="col-6 col-md-12 col-xl-6 d-flex align-items-center justify-content-center p-4">
             <a class="main_a " href="../img/services/carte_restaurant.pdf" target="_blank">
               <button class="btn btn-success">Découvrez la carte du restaurant</button>
             </a>
           </div>
 
         </div>
-      </section>
+      </section> 
     </div>
   </main>
   <footer>
@@ -215,10 +258,10 @@ $sethoraires = $horaires->fetchAll(PDO::FETCH_ASSOC);
       </div>
 
       <!--  -->
-    </section>
-  </footer>
+      </section>
+      </footer>
 
-  <script src="../js/services.js"></script>
+      <script src="../js/services.js"></script>
 </body>
 
 </html>

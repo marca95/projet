f28e0ca39fd9f16d0e04bf805afc6ba7378a7e23
Pierre Majerus <pierre.majerus@outlook.be>
2024-04-18 16:51:22 +0200

    update file mariadb

--- mariaDBzoo.sql
+++ mariaDBzoo.sql
@@ -14,11 +14,44 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
+
+-- Listage de la structure de la base pour zoo
+CREATE DATABASE IF NOT EXISTS `zoo` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci */;
+USE `zoo`;
+
+-- Listage de la structure de la table zoo. accueil_services
+CREATE TABLE IF NOT EXISTS `accueil_services` (
+  `id_service` int(11) NOT NULL,
+  `content` text NOT NULL,
+  `img1` varchar(255) NOT NULL,
+  `img2` varchar(255) NOT NULL,
+  `title_btn` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id_service`),
+  CONSTRAINT `accueil_services_ibfk_1` FOREIGN KEY (`id_service`) REFERENCES `services` (`id_service`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
 -- Listage des données de la table zoo.accueil_services : ~3 rows (environ)
 INSERT INTO `accueil_services` (`id_service`, `content`, `img1`, `img2`, `title_btn`) VALUES
 	(1, 'Une balade du domaine en petit train avec notre pilote expérimenté René', '../img/accueil/train.png', '../img/accueil/rene.jpeg', 'Pour plus d\'info'),
 	(2, 'Vous pourrez également visiter les animaux dans leurs habitats et vous balladez avec des\r\n              animaux inoffensifs,\r\n              telles que des chèvres, des moutons, des ânes pour le bonheur des petits comme des grands!', '../img/accueil/visite1.jpg', '../img/accueil/visite2.jpg', 'Pour plus d\'info'),
 	(3, 'Venez découvrir nos magnifiques plats concoctés par nos chefs qualifiés.\r\n              Vous y découvrirez une cuisine authentique, créative mais surtout des plats zéro déchet.\r\n              Nous utilisons le maximum de produits locaux afin de respecter notre conviction, <i>\r\n                une planète plus verte.</i>', '../img/accueil/resto1.jpg', '../img/accueil/resto2.jpg', 'Pour plus d\'info');
+
+-- Listage de la structure de la table zoo. animals
+CREATE TABLE IF NOT EXISTS `animals` (
+  `id_animal` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) DEFAULT NULL,
+  `type` varchar(50) NOT NULL,
+  `race` varchar(50) NOT NULL,
+  `id_location` int(11) NOT NULL,
+  `id_home` int(11) NOT NULL,
+  `root` varchar(255) DEFAULT NULL,
+  `commonName` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`id_animal`),
+  KEY `fk_id_location` (`id_location`),
+  KEY `fk_id_home` (`id_home`),
+  CONSTRAINT `fk_id_home` FOREIGN KEY (`id_home`) REFERENCES `homes` (`id_home`),
+  CONSTRAINT `fk_id_location` FOREIGN KEY (`id_location`) REFERENCES `locations` (`id_location`)
+) ENGINE=InnoDB AUTO_INCREMENT=113 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- Listage des données de la table zoo.animals : ~89 rows (environ)
 INSERT INTO `animals` (`id_animal`, `name`, `type`, `race`, `id_location`, `id_home`, `root`, `commonName`) VALUES
@@ -112,6 +145,18 @@ INSERT INTO `animals` (`id_animal`, `name`, `type`, `race`, `id_location`, `id_h
 	(88, 'Ken', 'hibou', 'Grand-duc', 2, 8, '../img/habitats/hibou.jpg', 'Le hibou'),
 	(89, 'Bastil', 'chouette', 'Hulotte', 1, 8, '../img/habitats/chouette.jpg', 'La chouette');
 
+-- Listage de la structure de la table zoo. avis
+CREATE TABLE IF NOT EXISTS `avis` (
+  `id_avis` int(11) NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(255) NOT NULL,
+  `content` text NOT NULL,
+  `status` varchar(10) DEFAULT NULL,
+  `id_employe` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id_avis`),
+  KEY `id_employe` (`id_employe`),
+  CONSTRAINT `avis_ibfk_1` FOREIGN KEY (`id_employe`) REFERENCES `users` (`id_user`)
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
 -- Listage des données de la table zoo.avis : ~4 rows (environ)
 INSERT INTO `avis` (`id_avis`, `first_name`, `content`, `status`, `id_employe`) VALUES
 	(1, 'Nathalie', 'Le Zoo d\'Arcadia est un espace où les visiteurs peuvent admirer des milliers d\'animaux dans un immense parc. L\'accueil est bon avec le cadre bienveillant et bien aménagé. Le zoo se distingue par sa grande diversité animalière avec des centaines et des centaines d\'espèces venues de tous les continents. Découverte et émerveillement sont donc les mots qui résument les avis des visiteurs du Zoo de Arcadia.', 'published', 12),
@@ -119,7 +164,29 @@ INSERT INTO `avis` (`id_avis`, `first_name`, `content`, `status`, `id_employe`) 
 	(13, 'Pierre', '3ieme commentaire', 'pending', 12),
 	(15, 'éé', 'éé', 'pending', NULL);
 
+-- Listage de la structure de la table zoo. contact
+CREATE TABLE IF NOT EXISTS `contact` (
+  `id_contact` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `DESCRIPTION` text NOT NULL,
+  PRIMARY KEY (`id_contact`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
 -- Listage des données de la table zoo.contact : ~0 rows (environ)
+
+-- Listage de la structure de la table zoo. foods
+CREATE TABLE IF NOT EXISTS `foods` (
+  `id_animal` int(11) NOT NULL,
+  `food` varchar(100) DEFAULT NULL,
+  `grams` float DEFAULT NULL,
+  `date_pass` datetime DEFAULT NULL,
+  `id_employed` int(11) DEFAULT NULL,
+  KEY `id_employed` (`id_employed`),
+  KEY `fk_id_animal` (`id_animal`),
+  CONSTRAINT `fk_id_animal` FOREIGN KEY (`id_animal`) REFERENCES `animals` (`id_animal`) ON DELETE CASCADE,
+  CONSTRAINT `foods_ibfk_2` FOREIGN KEY (`id_employed`) REFERENCES `users` (`id_user`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- Listage des données de la table zoo.foods : ~19 rows (environ)
 INSERT INTO `foods` (`id_animal`, `food`, `grams`, `date_pass`, `id_employed`) VALUES
@@ -143,6 +210,20 @@ INSERT INTO `foods` (`id_animal`, `food`, `grams`, `date_pass`, `id_employed`) V
 	(15, 'graine pour tortues ', 200, '2024-04-16 11:40:00', 12),
 	(64, 'Viande fraiche', 4000, '2024-04-17 15:21:00', 12);
 
+-- Listage de la structure de la table zoo. homes
+CREATE TABLE IF NOT EXISTS `homes` (
+  `id_home` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) NOT NULL,
+  `description` text DEFAULT NULL,
+  `main_root` varchar(255) NOT NULL,
+  `second_root` varchar(255) NOT NULL,
+  `url_img_accueil` varchar(255) NOT NULL,
+  `commonName` varchar(255) NOT NULL,
+  `second_title` varchar(255) NOT NULL,
+  PRIMARY KEY (`id_home`),
+  UNIQUE KEY `name` (`name`)
+) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
 -- Listage des données de la table zoo.homes : ~8 rows (environ)
 INSERT INTO `homes` (`id_home`, `name`, `description`, `main_root`, `second_root`, `url_img_accueil`, `commonName`, `second_title`) VALUES
 	(1, 'foret', 'Cette forêt s\'étend sur plus de 3000m² où se cachent de nombreux cervidés ainsi que quelques animaux de la ferme. Vous pouvez vous y balader en toute tranquillité, profiter du calme et des magnifiques hêtres de plus de 80 ans. Ces animaux sont inoffensifs. Merci de respecter le silence et de veiller à leur bien-être.', '../img/habitats/foret.jpg', '../img/habitats/foret1.jpg', '../img/accueil/foret.jpg', 'La fôret', 'La foret d\'Arcadia'),
@@ -154,6 +235,16 @@ INSERT INTO `homes` (`id_home`, `name`, `description`, `main_root`, `second_root
 	(7, 'oceanarium', 'Magnifique espace vitrée où se situe également un tunnel de + de 50m de long pour rentrer dans le monde de l\'océan. Cette espace est l\'une de plus grande fierté du patron car il habrite des\r\n              spécimen rare et très ancien.', '../img/habitats/oceanarium.jpg', '../img/habitats/oceanarium1.jpg', '../img/accueil/oceanarium.jpg', 'L\'océanarium', 'Le sous-marin'),
 	(8, 'voliere', 'Notre volière accueil jusqu\'à 50 espèces de volatiles différents. Ici, nous allons vous\r\n              montrez les animaux ayant le plus de succès auprès de nos visiteurs, mais croyez nous, tous, sont en très\r\n              bonne santé et vivent \'leur best life\'.\r\n', '../img/habitats/voliere.jpg', '../img/habitats/voliere1.jpg', '../img/accueil/voliere.jpg', 'La volière', 'Le ciel d\'Arcadia');
 
+-- Listage de la structure de la table zoo. horaires
+CREATE TABLE IF NOT EXISTS `horaires` (
+  `id_horaire` int(11) NOT NULL AUTO_INCREMENT,
+  `day_week` varchar(50) NOT NULL,
+  `start_time` varchar(50) DEFAULT NULL,
+  `end_time` varchar(50) DEFAULT NULL,
+  `is_closed` tinyint(1) NOT NULL,
+  PRIMARY KEY (`id_horaire`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
 -- Listage des données de la table zoo.horaires : ~7 rows (environ)
 INSERT INTO `horaires` (`id_horaire`, `day_week`, `start_time`, `end_time`, `is_closed`) VALUES
 	(1, 'Lundi', '', '', 1),
@@ -164,6 +255,13 @@ INSERT INTO `horaires` (`id_horaire`, `day_week`, `start_time`, `end_time`, `is_
 	(6, 'Samedi', '10h', '19h', 0),
 	(7, 'Dimanche', '11h', '20h', 0);
 
+-- Listage de la structure de la table zoo. locations
+CREATE TABLE IF NOT EXISTS `locations` (
+  `id_location` int(11) NOT NULL AUTO_INCREMENT,
+  `NAME` varchar(50) NOT NULL,
+  PRIMARY KEY (`id_location`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
 -- Listage des données de la table zoo.locations : ~6 rows (environ)
 INSERT INTO `locations` (`id_location`, `NAME`) VALUES
 	(1, 'Europe'),
@@ -173,17 +271,55 @@ INSERT INTO `locations` (`id_location`, `NAME`) VALUES
 	(5, 'Antarctique'),
 	(6, 'Océanie');
 
+-- Listage de la structure de la table zoo. roles
+CREATE TABLE IF NOT EXISTS `roles` (
+  `id_role` int(11) NOT NULL,
+  `name` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id_role`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
 -- Listage des données de la table zoo.roles : ~3 rows (environ)
 INSERT INTO `roles` (`id_role`, `name`) VALUES
 	(1, 'administrateur'),
 	(2, 'veterinaire'),
 	(3, 'employe');
 
+-- Listage de la structure de la table zoo. services
+CREATE TABLE IF NOT EXISTS `services` (
+  `id_service` int(11) NOT NULL AUTO_INCREMENT,
+  `main_title` varchar(255) NOT NULL,
+  `second_title` varchar(255) DEFAULT NULL,
+  `img_root` varchar(255) NOT NULL,
+  `content` text NOT NULL,
+  `third_title` varchar(255) DEFAULT NULL,
+  `second_content` text DEFAULT NULL,
+  `NAME` varchar(255) NOT NULL,
+  `link_class` varchar(255) DEFAULT NULL,
+  `link_url` varchar(255) DEFAULT NULL,
+  `img_root_link` varchar(255) DEFAULT NULL,
+  `btn_class` varchar(255) DEFAULT NULL,
+  `btn_url` varchar(255) DEFAULT NULL,
+  `btn_title` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id_service`)
+) ENGINE=InnoDB AUTO_INCREMENT=22 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
 -- Listage des données de la table zoo.services : ~3 rows (environ)
 INSERT INTO `services` (`id_service`, `main_title`, `second_title`, `img_root`, `content`, `third_title`, `second_content`, `NAME`, `link_class`, `link_url`, `img_root_link`, `btn_class`, `btn_url`, `btn_title`) VALUES
 	(1, 'Petit tour en train', 'Tarifs pour 45min', '../img/services/train1.jpg', '            -> Adulte: 8€\r\n            <br />\r\n            -> Enfant de 4 à 12ans: 5€\r\n            <br />\r\n            -> Enfant de - 4ans: Gratuit\r\n            <br />\r\n            -> Place réservé pour personne à mobilité reduite', 'Durée :', 'Le train roulera de 11h à 17h\r\n            La promenade dure +/- 45min.\r\n            Le départ se fera à coté de l\'accueil principal.\r\n            Elle démarrera a toutes les heures pleines de 11h à 18h du mercredi au dimanche.\r\n            En fonction des conditions climatique, il est possible que le petit train reste au chaud. L\'accueil vous\r\n            préviendra lors de votre arrivé.', 'train', NULL, NULL, NULL, NULL, NULL, NULL),
 	(2, 'Visite des habitats', '', '../img/services/guide.jpg', 'Rendez-vous avec notre guide Arcadia tous les jours de la semaine de 10h30 à 12h et de 14h à\r\n          16h.\r\n          Vous aurez la chance d\'accéder à notre volière où vivent des centaines de différentes sortent\r\n          d\'oiseaux.\r\n          Vous découvrirez sous un autre angle les animaux australiens comme le kangourous, ainsi que\r\n          quelques animaux africains comme les samïris, la\r\n          girafe ou les gorilles. <b>Activité gratuite !</b>', '', 'Il est <b>strictement interdit </b> de donner de la nourriture aux animaux pendant la visite. La nourriture\r\n          est soigneusement préparée et pesée par nos soigneurs. Une quantité trop élévé de nourriture non autorisé\r\n          peut blesser l\'animal, même pire dans certain cas. Merci d\'écouter attentivement les consignes de sécurité\r\n          du guide durant la visite.', 'habitat', NULL, NULL, NULL, NULL, NULL, NULL),
 	(3, 'Restaurant', '', '../img/services/restaurant.jpg', '          Notre magnifique restaurant avec 200 places intérieur et 50 places extérieur avec vue sur notre lac, vous\r\n          accompagne de 11h à 15h tous les jours de la semaine.', '', '          Nous vous proposons une cuisine 0 déchet et où nous favorisons les produits locaux. Des assiettes saine,\r\n          riche et savoureuse préparé par nos chefs\r\n          Le restaurant ressemble à notre physionomie: Mettre tout en notre pouvoir pour contribuer à un planète plus\r\n          verte pour le biens de nos animaux.', 'resto', 'col-6 col-md-12 col-xl-6 d-flex align-items-center justify-content-center p-4', './terre.php', '../img/accueil/ecologie.jpg', 'col-6 col-md-12 col-xl-6 d-flex align-items-center justify-content-center p-4', '../img/services/carte_restaurant.pdf', 'Découvrez la carte du restaurant');
+
+-- Listage de la structure de la table zoo. states
+CREATE TABLE IF NOT EXISTS `states` (
+  `id_animal` int(11) NOT NULL,
+  `state` text DEFAULT NULL,
+  `detail` text DEFAULT NULL,
+  `id_vete` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id_animal`),
+  KEY `id_vete` (`id_vete`),
+  CONSTRAINT `fk_animals_id` FOREIGN KEY (`id_animal`) REFERENCES `animals` (`id_animal`) ON DELETE CASCADE,
+  CONSTRAINT `states_ibfk_2` FOREIGN KEY (`id_vete`) REFERENCES `users` (`id_user`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- Listage des données de la table zoo.states : ~21 rows (environ)
 INSERT INTO `states` (`id_animal`, `state`, `detail`, `id_vete`) VALUES
@@ -209,11 +345,41 @@ INSERT INTO `states` (`id_animal`, `state`, `detail`, `id_vete`) VALUES
 	(88, 'Très belle animal, agréable et très joviale', '', 11),
 	(89, 'Très bon animal, très doux ', 'Pas mal de perte de plumes à faire attention', 11);
 
+-- Listage de la structure de la table zoo. status_home
+CREATE TABLE IF NOT EXISTS `status_home` (
+  `id_home` int(11) NOT NULL,
+  `opinion_state` text DEFAULT NULL,
+  `improvement` text DEFAULT NULL,
+  `id_veto` int(11) DEFAULT NULL,
+  KEY `fk_id_home_status_home` (`id_home`),
+  KEY `fk_id_veto_status_home` (`id_veto`),
+  CONSTRAINT `fk_id_home_status_home` FOREIGN KEY (`id_home`) REFERENCES `homes` (`id_home`) ON DELETE CASCADE,
+  CONSTRAINT `fk_id_veto_status_home` FOREIGN KEY (`id_veto`) REFERENCES `users` (`id_user`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
 -- Listage des données de la table zoo.status_home : ~3 rows (environ)
 INSERT INTO `status_home` (`id_home`, `opinion_state`, `improvement`, `id_veto`) VALUES
 	(2, 'Filtrer l\'eau plus régulièrement.', 'Prévoir un planning mensuel du filtrage', 11),
 	(4, 'rectif', 'rectifaction', 11),
 	(1, 'Magnifique endroit pour s\'y balader. Plusieurs variétés d\'arbres. ', 'Aménager un chemin route plus "safe"', 11);
+
+-- Listage de la structure de la table zoo. users
+CREATE TABLE IF NOT EXISTS `users` (
+  `id_user` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) NOT NULL,
+  `first_name` varchar(50) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `password` varchar(100) DEFAULT NULL,
+  `id_role` int(11) NOT NULL,
+  `birthday` date NOT NULL,
+  `hire` date NOT NULL,
+  PRIMARY KEY (`id_user`),
+  UNIQUE KEY `username` (`username`),
+  UNIQUE KEY `email` (`email`),
+  KEY `id_role` (`id_role`),
+  CONSTRAINT `users_ibfk_1` FOREIGN KEY (`id_role`) REFERENCES `roles` (`id_role`)
+) ENGINE=InnoDB AUTO_INCREMENT=43 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- Listage des données de la table zoo.users : ~7 rows (environ)
 INSERT INTO `users` (`id_user`, `name`, `first_name`, `username`, `email`, `password`, `id_role`, `birthday`, `hire`) VALUES

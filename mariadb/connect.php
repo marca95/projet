f96a4170c7455ca5b34c43d362b953b9ba154925
Pierre Majerus <pierre.majerus@outlook.be>
2024-04-22 12:11:27 +0200

    update var env in connect.php

--- mariadb/connect.php
+++ mariadb/connect.php
@@ -2,8 +2,8 @@
 
 try {
   // With Heroku
-  if (getenv('JAWSDB_URL') !== false) {
-    $dbparts = parse_url(getenv('JAWSDB_URL'));
+  if (getenv('JAWSDB') !== false) {
+    $dbparts = parse_url(getenv('JAWSDB'));
 
     $hostname = $dbparts['host'];
     $username = $dbparts['user'];

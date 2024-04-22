58a13151b2110c39757fa10a7d48d6715d9c1b13
Pierre Majerus <pierre.majerus@outlook.be>
2024-04-22 12:31:59 +0200

    update file connect

--- mariadb/connect.php
+++ mariadb/connect.php
@@ -1,23 +1,24 @@
 <?php
 
+if (getenv('JAWSDB_URL') !== false) {
+  $dbparts = parse_url(getenv('JAWSDB_URL'));
+
+  $hostname = $dbparts['host'];
+  $port = $dbparts['port'];
+  $username = $dbparts['user'];
+  $password = $dbparts['pass'];
+  $database = ltrim($dbparts['path'], '/');
+} else {
+  $username = 'root';
+  $password = 'pierre2';
+  $hostname = 'localhost';
+  $database = 'zoo';
+  $port = '5353';
+}
+
 try {
-  // With Heroku
-  if (getenv('JAWSDB_URL') !== false) {
-    $dbparts = parse_url(getenv('JAWSDB_URL'));
-
-    $hostname = $dbparts['cxmgkzhk95kfgbq4.cbetxkdyhwsb.us-east-1.rds.amazonaws.com'];
-    $username = $dbparts['zvv65igt98n68w6t'];
-    $password = $dbparts['cxmgkzhk95kfgbq4'];
-    $database = ltrim($dbparts['coi8vzror4dadlrt'], '/');
-  } else {
-    // Connect DB
-    $userDB = 'root';
-    $passwordDB = 'pierre2';
-
-    $pdo = new PDO('mysql:host=localhost;port=5353;dbname=zoo', $userDB, $passwordDB);
-    // Gestion des erreurs
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  }
+  $pdo = new PDO('mysql:host=' . $hostname . ';port=' . $port . ';dbname=' . $database, $username, $password);
+  $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
   echo "Erreur : " . $e->getMessage();
 }

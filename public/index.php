ca66e6235f9948435d2f6f73d33b98cebc33dd3d
Pierre Majerus <pierre.majerus@outlook.be>
2024-04-22 14:12:13 +0200

    test

--- public/index.php
+++ public/index.php
@@ -2,7 +2,36 @@
 <html lang="en">
 
 <?php
-require_once '../mariadb/connect.php';
+
+
+<?php
+
+use PDO;
+
+if (getenv('JAWSDB_URL') !== false) {
+  $dbparts = parse_url(getenv('JAWSDB_URL'));
+
+  $hostname = $dbparts['host'];
+  $port = $dbparts['port'];
+  $username = $dbparts['user'];
+  $password = $dbparts['pass'];
+  $database = ltrim($dbparts['path'], '/');
+} else {
+  $username = 'root';
+  $password = 'pierre2';
+  $hostname = 'localhost';
+  $database = 'zoo';
+  $port = '5353';
+}
+
+try {
+  $pdo = new PDO('mysql:host=' . $hostname . ';port=' . $port . ';dbname=' . $database, $username, $password);
+  $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+  echo "Erreur : " . $e->getMessage();
+}
+
+
 ?>
 
 <head>

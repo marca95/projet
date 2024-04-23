c98d5a3615747b833c27a96931bfd4e717737c8c
Pierre Majerus <pierre.majerus@outlook.be>
2024-04-23 13:52:27 +0200

    error in console habitats

--- public/index.php
+++ public/index.php
@@ -28,7 +28,7 @@ $accueilservices = $viewServices->fetchAll(PDO::FETCH_ASSOC);
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
   <link href="./style/css/accueil.css" rel="stylesheet">
   <link href="./style/font/font.css" rel="stylesheet">
-  <link href="./img/logo.png" rel="icon">
+  <link href="./img/accueil/logo.png" rel="icon">
 </head>
 
 <body>

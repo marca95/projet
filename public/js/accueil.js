8895624d7ea88c1de6c24fab11e400eac04bd2bd
marca95 <pierre.majerus@outlook.be>
2024-05-07 15:16:50 +0200

    Update connection, if already logged in, direct access to your space

--- public/js/accueil.js
+++ public/js/accueil.js
@@ -1,7 +1,7 @@
 const links = document.querySelectorAll('nav li');
 
 icon.addEventListener("click", () => {
- nav.classList.toggle("active");
+  nav.classList.toggle("active");
 })
 
 links.forEach((link) => {
@@ -9,3 +9,16 @@ links.forEach((link) => {
     nav.classList.remove("active");
   });
 });
+
+// Manage nav li connect/space member
+
+let connected = document.querySelector('.connected');
+let disconnected = document.querySelector('.disconnected');
+
+if (isset($_SESSION['id_user'])) {
+  connected.classList.add('d-block');
+  disconnected.classList.remove('d-none');
+} else {
+  connected.classList.add('d-none');
+  disconnected.classList.remove('d-block');
+}

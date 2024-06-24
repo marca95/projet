bccd42d3d1849b6334485abdc5defa9079671c9a
marca95 <pierre.majerus@outlook.be>
2024-06-24 14:31:12 +0200

    update

--- mariadb/disconnect.php
+++ mariadb/disconnect.php
@@ -2,9 +2,7 @@
 // btn logout session
 if (isset($_POST['logout'])) {
   session_destroy();
-  setcookie("id_user", "", time() - 3600, '', '', true, false);
-  // setcookie("username", "", time() - 3600, '', '', true, true);
-  // setcookie("token", "", time() - 3600, '', '', true, true);
+  setcookie("id_user", "", time() - 3600, '/', 'zoo-arcadia-2024-7efa0677447b.herokuapp.com', true, false);
   header("Location: connexion.php");
   exit();
 }

6576da18eb81365b59f50879231eb9e61acfec7c
marca95 <pierre.majerus@outlook.be>
2024-06-25 10:02:50 +0200

    update

--- public/veterinaire.php
+++ public/veterinaire.php
@@ -4,7 +4,6 @@ session_start();
 include_once '../mariadb/connect.php';
 include_once '../mariadb/login_veterinaire.php';
 include_once '../mariadb/stmt.php';
-// include_once '../mariadb/states.php';
 include_once '../mariadb/disconnect.php';
 require_once '../mariadb/cleanCookies.php';
 

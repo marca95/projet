858d1b41a45354ce878a5e10084fd6684e4d0a13
marca95 <pierre.majerus@outlook.be>
2024-07-02 11:44:57 +0200

    update

--- tests/AnimalRepositoryDeleteTest.php
+++ tests/AnimalRepositoryDeleteTest.php
@@ -12,11 +12,14 @@ class AnimalRepositoryDeleteTest extends TestCase
 {
   public function testDeleteAnimalSuccessfullyDeletes()
   {
-    /*
-    * Créez des mocks pour 'Collection' et 'DeleteResult' pour simuler leur 
-    * comportement sans dépendre d'une véritable base de données MongoDB
-    */
-    // Créer un mock pour la collection MongoDB
+    /**
+     * 
+     * Créez des mocks pour 'Collection' et 'DeleteResult' pour simuler leur 
+     * comportement sans dépendre d'une véritable base de données MongoDB
+     * 
+     * Créer un mock pour la collection MongoDB
+     */
+
     $collection = $this->createMock(Collection::class);
     // Créer un mock pour le résultat de la suppression
     $deleteResult = $this->createMock(DeleteResult::class);
@@ -35,16 +38,12 @@ class AnimalRepositoryDeleteTest extends TestCase
 
   public function testDeleteAnimalNoMatchFound()
   {
-    // Créer un mock pour la collection MongoDB
     $collection = $this->createMock(Collection::class);
-    // Créer un mock pour le résultat de la suppression
     $deleteResult = $this->createMock(DeleteResult::class);
 
-    // Configurer le mock pour retourner aucun document supprimé
     $deleteResult->method('getDeletedCount')->willReturn(0);
     $collection->method('deleteOne')->willReturn($deleteResult);
 
-    // Instancier AnimalManager avec la collection mockée
     $repository = new AnimalManager($collection);
 
     $result = $repository->deleteAnimal('Chou', 'Fleur');

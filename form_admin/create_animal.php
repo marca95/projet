2fdbcfd7be7ddef0fec0f88a567fe05b857bfcbd
marca95 <pierre.majerus@outlook.be>
2024-07-05 16:30:33 +0200

    amelioration

--- form_admin/create_animal.php
+++ form_admin/create_animal.php
@@ -11,6 +11,7 @@ $recuplocations->execute();
 $optionsLocations = $recuplocations->fetchAll(PDO::FETCH_ASSOC);
 
 
+
 if (isset($_POST['createNewAnimal']) && isset($_FILES['upload']) && $_FILES['upload']['error'] === 0) {
   $messageAnimal = '';
 
@@ -23,28 +24,37 @@ if (isset($_POST['createNewAnimal']) && isset($_FILES['upload']) && $_FILES['upl
   $commonName = htmlspecialchars($_POST['commonName']);
   $destination = "./img/habitats/" . $_FILES['upload']['name'];
 
-  $newAnimal = $pdo->prepare('INSERT INTO animals(name, type, race, id_location, id_home, root, commonName) 
-  VALUES (:name, :type, :race, :id_location, :id_home, :root, :commonName)');
-  $newAnimal->bindValue(':name', $name);
-  $newAnimal->bindValue(':type', $type);
-  $newAnimal->bindValue(':race', $race);
-  $newAnimal->bindValue(':id_location', $location);
-  $newAnimal->bindValue(':id_home', $home);
-  $newAnimal->bindValue(':root', $destination);
-  $newAnimal->bindValue(':commonName', $commonName);
-
-  // Fichier de max 2MB
-  $maxFileSize = 2 * 1024 * 1024;
-  $allowedExtensions = ['jpg', 'jpeg', 'png', 'gif'];
-  $fileExtension = strtolower(pathinfo($_FILES['upload']['name'], PATHINFO_EXTENSION));
-
-  if (move_uploaded_file($imgRoot, $destination) && $_FILES['upload']['size'] < $maxFileSize && in_array($fileExtension, $allowedExtensions)) {
-    if ($newAnimal->execute()) {
-      $messageAnimal = 'Nouvel animal créé avec succès';
-    } else {
-      $messageAnimal = 'Erreur lors de la création du nouvel animal';
-    }
+  $maxFileSize = 2 * 1024 * 1024; // 2MB
+  if ($_FILES['upload']['size'] > $maxFileSize) {
+    $messageAnimal = "Fichier trop volumineux.";
   } else {
-    $messageAnimal = 'Erreur lors du téléchargement de l\'image';
+    $allowedExtensions = ['jpg', 'jpeg', 'png', 'gif'];
+    $fileExtension = strtolower(pathinfo($_FILES['upload']['name'], PATHINFO_EXTENSION));
+
+    if (!in_array($fileExtension, $allowedExtensions)) {
+      $messageAnimal = "Extension de fichier non autorisée.";
+    } else {
+      if (move_uploaded_file($imgRoot, $destination)) {
+        $newAnimal = $pdo->prepare('INSERT INTO animals(name, type, race, id_location, id_home, root, commonName) 
+                    VALUES (:name, :type, :race, :id_location, :id_home, :root, :commonName)');
+        $newAnimal->bindValue(':name', $name);
+        $newAnimal->bindValue(':type', $type);
+        $newAnimal->bindValue(':race', $race);
+        $newAnimal->bindValue(':id_location', $location);
+        $newAnimal->bindValue(':id_home', $home);
+        $newAnimal->bindValue(':root', $destination);
+        $newAnimal->bindValue(':commonName', $commonName);
+
+        if ($newAnimal->execute()) {
+          $messageAnimal = 'Nouvel animal créé avec succès';
+        } else {
+          $messageAnimal = 'Erreur lors de la création du nouvel animal';
+        }
+      } else {
+        $messageAnimal = 'Erreur lors du téléchargement de l\'image';
+      }
+    }
   }
+} else {
+  $messageAnimal = 'Erreur lors du téléchargement de l\'image';
 }
